476a9a29b545b81d20446cd98a0e932b57ea9319
jissykakin <sicaji22@gmail.com>
2026-02-17 08:54:53 +0530

    add docs files

---
+++ docs/assets/PortfolioPage-Cl92tf0w.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CZqBB4Ee.js";import{U as r}from"./UnderConstructionPage-DlUr4B2K.js";const e=()=>o.jsx(r,{});export{e as default};
---
+++ docs/assets/ServicesPage-DSkcGBBJ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CZqBB4Ee.js";import{U as t}from"./UnderConstructionPage-DlUr4B2K.js";const s=()=>r.jsx(t,{});export{s as default};
